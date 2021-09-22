f70048cf64fcadeec3ddcff90b3d8c22324b7a88
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-21 18:41:06 -0700

    forgot I was on the main branch, but mad some changes to the search container, trying to build it out to have a rendered list of artists

--- src/containers/ArtistSearchContainer.jsx
+++ src/containers/ArtistSearchContainer.jsx
@@ -1,10 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import { getArtists } from '../services/artistApiSearch';
+import { getAllArtists, getArtistsByQuery, getArtistMusicById } from '../services/artistApiSearch';
 import ArtistList from '../components/artists/ArtistList';
 
 export const ArtistSearch = () => {
-  // const [ loading, setLoading ] = useState(true);
+  const [ loading, setLoading ] = useState(true);
+  // const [ query, setQuery ] = useState('');
+  const [ artists, setArtists ] = useState([]);
 
-  return <h1>Loading...</h1>;
+  // useEffect(() => (), [])
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <ArtistList
+      artists={artists}
+    />
+  );
 };
 
