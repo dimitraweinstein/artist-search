89c864f07999e04948afe6fb1b153098916a04f0
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-21 17:54:26 -0700

    built out fetch calls, updated artist component based on few changes to service functions

--- src/services/artistApiSearch.js
+++ src/services/artistApiSearch.js
@@ -1,44 +1,67 @@
 
-export const getArtists = async () => {
-  const artistArray = [];
+export const getAllArtists = async () => {
   try {
     const res = await fetch(
-      'http://musicbrainz.org/ws/2/artist?query=%3CSEARCH%3E&fmt=json&limit=25'
+      'https://musicbrainz.org/ws/2/artist?query=?&fmt=json&limit=25'
     );
-    const artists = await res.json();
-    
-    const res2 = await fetch(
-      `http://musicbrainz.org/ws/2/release?artist=${artists.artist.id}&fmt=json`
-    );
+    const allArtists = await res.json();
 
-    const albums = await res2.json();
-
-    return artists.map((artist) => ({
+    return allArtists.map((artist) => ({
       id: artist.id,
       name: artist.name,
+      description: artist.disambiguation,
+      genre: artist.tags[ 0 ].name,
       country: artist.area.name,
-      genre: artist.disambiguation,
-    })), albums.map((album) => ({
-      artistId: artists.artist.id,
-      albumId: album.releases.id,
-      title: album.releases.title,
-      releaseYear: album.releases.date
-    })).push(artistArray);
-    
-      
-      
-      
+    }));
   } catch (error) {
     console.error((`Error getting artist: ${error.message}`));
-    return [] || artistArray;
+    return [];
   }
 };
 
-// ** This will be used to map over artistArray once all fetch calls are made.**
-// .then(artists.map((artist) => ({
-//   id: artist.id,
-//   name: artist.name,
-//   country: artist.area.name,
-//   genre: artist.disambiguation,
-//   // Albums Data added to artist object here.
-// })));
+export const getArtistsByQuery = async (artistQuery) => {
+  try {
+    const res = await fetch(
+      // eslint-disable-next-line max-len
+      `http://musicbrainz.org/ws/2/artist?query=${artistQuery}&fmt=json&limit=25`
+    );
+    const artists = await res.json();
+  
+    return artists.map((artist) => ({
+      id: artist.id,
+      name: artist.name,
+      description: artist.disambiguation,
+      country: artist.area.name,
+    }));
+  } catch (error) {
+    console.error((`Error getting artist: ${error.message}`));
+    return [];
+  }
+};
+
+export const getArtistMusicById = async (artistId) => {
+  try {
+    const res = await fetch(
+      `http://musicbrainz.org/ws/2/release?artist=${artistId}&fmt=json`
+    );
+    const albums = await res.json();
+
+    const res2 = await fetch(
+      `https://coverartarchive.org/release/${releaseId}/front`
+    );
+    const coverArt = await res2.json();
+
+    albums.map((album) => ({
+      id: album.releases.id,
+      title: album.releases.title,
+      releaseDate: album.releases.date,
+      coverArt: coverArt.map((cover) => ({
+        id: cover.releases.id,
+      }))
+    }));
+  
+  } catch (error) {
+    console.error((`Error getting artist: ${error.message}`));
+    return [];
+  }
+};
